fce067de79fbc99db70d6feb1222b9618519cac1
REDOWAN038 <redowanibrahim2724@gmail.com>
2025-07-18 12:11:18 +0600

    showing location and implemented location switcher

--- ecovista/app/[location]/page.js
+++ ecovista/app/[location]/page.js
@@ -1,7 +1,11 @@
+import LocationInfo from "@/components/LocationInfo"
 import React from "react"
 
-const LocationPage = () => {
-    return <div>LocationPage</div>
+const LocationPage = ({
+    params: { location },
+    searchParams: { latitude, longitude },
+}) => {
+    return <LocationInfo lat={latitude} lon={longitude} />
 }
 
 export default LocationPage
